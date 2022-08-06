c25ff791b968b39b38bd20c0af1fd3d85ae1f2b4
SokNgam <106458746+SokNgam@users.noreply.github.com>
2022-08-06 16:50:30 +0700

    Gh-official

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+$(".login").click(function(){
+    $(".main-user-login").slideDown(200);
+});
+$("#close-user-login").click(function(){
+    $(".main-user-login").slideUp(200);
+});
+$(".register").click(function(){
+    $(".main-user-register").slideDown(200);
+});
+$("#close-user-register").click(function(){
+    $(".main-user-register").slideUp(200);
+});
